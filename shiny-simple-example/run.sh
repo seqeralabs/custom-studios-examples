11ce554d7e9a57654919ce84e7bbed2e09b4db00
Jonathan Manning <jonathan.manning@seqera.io>
2025-05-01 14:41:01 +0100

    Improve run.sh script with proper connect-client handling

--- shiny-simple-example/run.sh
+++ shiny-simple-example/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Set the port based on environment
+# Set port based on environment
 PORT=${CONNECT_TOOL_PORT:-3000}
 
-# Run the app
+# Define the Shiny command
+SHINY_CMD="shiny::runApp('/app/app_plot_demo.R', host='0.0.0.0', port=${PORT})"
+
+# Run the appropriate command based on environment
 if [ -n "$CONNECT_TOOL_PORT" ]; then
-    # Run with connect-client entrypoint (for Seqera Studios)
-    exec /usr/bin/connect-client --entrypoint micromamba run -n shiny R -e "shiny::runApp('/app/app_plot_demo.R', host='0.0.0.0', port=$PORT)"
+    # In Seqera Studios
+    exec /usr/bin/connect-client --entrypoint "micromamba run -n shiny R -e '${SHINY_CMD}'"
 else
-    # Run directly for local testing
-    exec micromamba run -n shiny R -e "shiny::runApp('/app/app_plot_demo.R', host='0.0.0.0', port=$PORT)"
+    # Local testing
+    micromamba run -n shiny R -e "${SHINY_CMD}"
 fi 
